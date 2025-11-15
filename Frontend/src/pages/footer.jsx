3e6df6a3f96888912e5ab17d01aa9dbc226dfea5
Prathamesh Sharad Mule <159987235+prathameshsmule@users.noreply.github.com>
2025-11-15 21:27:39 +0530

    Update footer.jsx

--- Frontend/src/pages/footer.jsx
+++ Frontend/src/pages/footer.jsx
@@ -54,6 +54,7 @@ const Footer = () => {
   className={styles.logoworknai}
   style={{
     fontWeight: "800",
+    margin-top:"5px",
     background: "linear-gradient(135deg, #ff3fbf 0%, #3f7bff 100%)",
     WebkitBackgroundClip: "text",
     WebkitTextFillColor: "transparent",
