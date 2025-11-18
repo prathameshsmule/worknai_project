7af61200f795eda61c2ab29855eb6c289b2df339
Prathamesh Sharad Mule <159987235+prathameshsmule@users.noreply.github.com>
2025-11-18 15:03:59 +0530

    Update footer.jsx

--- Frontend/src/pages/footer.jsx
+++ Frontend/src/pages/footer.jsx
@@ -200,32 +200,15 @@ const Footer = () => {
           <div className={`${styles.footerSection} ${styles.socialSection}`}>
             <div className={styles.socialIcons}>
               <a
-                href="#facebook"
+                href="https://www.facebook.com/share/1Ecu97wj36/"
                 className={`${styles.socialIcon} ${styles.facebook}`}
               >
                 <Facebook size={24} />
               </a>
+              
+             
               <a
-                href="#twitter"
-                className={`${styles.socialIcon} ${styles.twitter}`}
-              >
-                <svg
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                >
-                  <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
-                </svg>
-              </a>
-              <a
-                href="#youtube"
-                className={`${styles.socialIcon} ${styles.youtube}`}
-              >
-                <Youtube size={24} />
-              </a>
-              <a
-                href="#instagram"
+                href="https://www.instagram.com/worknai.in?igsh=MTQyMHRneG4wdGswdA=="
                 className={`${styles.socialIcon} ${styles.instagram}`}
               >
                 <Instagram size={24} />
